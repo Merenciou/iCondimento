2a9a4ea3517ec8e4dca393047ee0b7277e74efd7
Merenciou <vitor.merencio.2012@hotmail.com>
2024-02-09 15:58:55 -0300

    Adição de imagem e reajuste do layout de descrição do tempero

--- lib/tempero_view.dart
+++ lib/tempero_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:i_condimento/homepage.dart';
+import 'package:i_condimento/temperos_page.dart';
 
 class TemperoView extends StatefulWidget {
-  const TemperoView({Key? key, required this.tempero, imgIndex}) : super(key: key);
+  const TemperoView({Key? key, required this.tempero}) : super(key: key);
   final Map<String, dynamic> tempero;
 
   @override
@@ -11,11 +13,16 @@ class TemperoView extends StatefulWidget {
 
 class _TemperoView extends State<TemperoView> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'sal',
+          widget.tempero['name'],
           style: GoogleFonts.montserrat(),
         ),
       ),
@@ -26,63 +33,91 @@ class _TemperoView extends State<TemperoView> {
             children: [
               Container(
                 width: double.infinity,
-                height: 250,
+                height: 150,
                 decoration: BoxDecoration(
                   color: Theme.of(context).colorScheme.primary,
                   borderRadius: const BorderRadius.all(
                     Radius.circular(15),
                   ),
                 ),
-                //todo: coloca a imagem aqui
-                // child: Image.asset(
-                //   widget.tempero['img'],
-                // ),
-              ),
-              Text(
-                widget.tempero['name'],
-                style: GoogleFonts.montserrat(fontSize: 20),
+                child: Image.asset(
+                    'lib/assets/images/${temperosIndex ? temperoPath[0] : temperoPath[1]}/${widget.tempero['imagePath']}.png'),
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Para que serve:',
-                      style: GoogleFonts.montserrat(fontWeight: FontWeight.w500),
-                    ),
-                    SizedBox(
-                      width: 150,
-                      child: Text(
-                        widget.tempero['function'],
-                        style: GoogleFonts.montserrat(fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                  ],
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  widget.tempero['name'],
+                  style: GoogleFonts.montserrat(fontSize: 20),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 200,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Como usar:',
-                        style: GoogleFonts.montserrat(fontWeight: FontWeight.w500),
-                      ),
-                      SizedBox(
-                        width: 150,
-                        child: Text(
-                          widget.tempero['how'],
-                          style: GoogleFonts.montserrat(fontWeight: FontWeight.w500),
-                        ),
-                      ),
-                    ],
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Text(
+                  'Função',
+                  style: GoogleFonts.montserrat(fontWeight: FontWeight.w500),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    color: Color(0xFFEEEEEE),
+                    borderRadius: BorderRadius.all(Radius.circular(15))),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 10),
+                    child: Text(
+                      widget.tempero['function'],
+                      style: GoogleFonts.montserrat(),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Text(
+                  'Ideal',
+                  style: GoogleFonts.montserrat(fontWeight: FontWeight.w500),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    color: Color(0xFFEEEEEE),
+                    borderRadius: BorderRadius.all(Radius.circular(15))),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 10),
+                    child: Text(
+                      widget.tempero['ideal'],
+                      style: GoogleFonts.montserrat(),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Text(
+                  'Como usar',
+                  style: GoogleFonts.montserrat(fontWeight: FontWeight.w500),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    color: Color(0xFFEEEEEE),
+                    borderRadius: BorderRadius.all(Radius.circular(15))),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Center(
+                    child: Text(
+                      widget.tempero['how'],
+                      style: GoogleFonts.montserrat(),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ),
               )
